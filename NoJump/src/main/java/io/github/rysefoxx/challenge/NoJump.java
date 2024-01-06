5eb08f600b51ea8cbefcf38b3671458a0de826a2
Rysefoxx <rysepvp1@gmail.com>
2024-01-06 18:04:50 +0100

    Added NoJump Challenge

---
+++ NoJump/src/main/java/io/github/rysefoxx/challenge/NoJump.java
@@ -0,0 +1,51 @@
+package io.github.rysefoxx.challenge;
+
+import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+import io.github.rysefoxx.core.ChallengePlugin;
+import io.github.rysefoxx.core.challenge.AbstractChallengeModule;
+import io.github.rysefoxx.core.challenge.ChallengeType;
+import io.github.rysefoxx.core.service.IChallengeService;
+import io.github.rysefoxx.core.service.IMessageService;
+import io.github.rysefoxx.core.util.ItemBuilder;
+import io.github.rysefoxx.core.util.StringUtil;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Rysefoxx
+ * @since 06.01.2024
+ */
+public class NoJump extends AbstractChallengeModule implements Listener, IChallengeService {
+
+    public NoJump() {
+        super("no_jump", ChallengeType.CHALLENGE);
+    }
+
+    @Override
+    public void onEnable(@NotNull ChallengePlugin plugin) {
+    }
+
+    @EventHandler
+    public void onPlayerJump(@NotNull PlayerJumpEvent event) {
+        if (!isEnabled()) return;
+        if (!isTimerEnabled()) return;
+        Player player = event.getPlayer();
+        if (ignore(player)) return;
+
+        end(player);
+    }
+
+    @Override
+    public @NotNull ItemStack displayItem(@NotNull Player player, @NotNull IMessageService messageService) {
+        return ItemBuilder.of(Material.valueOf(messageService.getTranslatedMessageLegacy(player, this.id + "_material")))
+                .displayName(messageService.getTranslatedMessage(player, this.id + "_displayname").append(Component.text(" ")).append(messageService.getTranslatedMessage(player, "enabled_" + isEnabled())))
+                .lore(StringUtil.splitStringAsComponent(messageService.getTranslatedMessageLegacy(player, this.id + "_lore")))
+                .build();
+    }
+}
